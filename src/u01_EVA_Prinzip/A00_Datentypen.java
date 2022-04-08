29da02460034fcf77326daba058bdf9a8a74e76e
YetiHafen <yetihafen734@gmail.com>
2022-04-08 18:36:23 +0200

    remove code

--- src/u01_EVA_Prinzip/A00_Datentypen.java
+++
@@ -1,9 +0,0 @@
-package u01_EVA_Prinzip;
-
-public class A00_Datentypen {
-
-    public static void main(String[] args) {
-        System.out.println("47 + 11");
-        System.out.println(47 + 11);
-    }
-}
